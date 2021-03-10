eb989c4255465af5625f1e27a065ae53a917f85a
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-10 15:49:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-choix = {1:"départ",2:"bidule"}
-x= int(input())
-
+print("Bienvenue a ce créateur de jeu dont vous etes le héro !. Vous pouvez créer un jeu style RPG !")
+print("Commencons par le debut, determinons les stats de debut des personnages sans bonus :")
+agi=int(input("Pour agilité ? (agi)"))
+def=int(input("Pour défense ? (def)"))
+strength=int(input("Pour strength ? (str)"))
+luck=int(input("Pour luck ? (luck)"))
+health=int(input("Pour health ? (HP)"))
+  
