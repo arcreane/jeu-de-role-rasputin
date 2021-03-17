bf70237535f25c5b998885989b23036d19bede04
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-17 15:54:02 +0100

    Update editables.py

--- editables.py
+++ editables.py
@@ -1,14 +1,22 @@
-name=input("Votre nom ?")
+name=input("Votre nom ?") 
+#Nom du profile account + Nom a montrer dans le game intro
+
 health=int(input("Pour health ? (HP)")
 invplace=int(input("Nombre de place dans l'inventaire ?")
-#Make list
-cash=int(input("Quantite d'argent au debut du jeu ?"))		 
+#List de ce size la
+cash=int(input("Quantite d'argent au debut du jeu ?"))
+#Option shop pour item
+
 agi=int(input("Pour agilité ? (agi)"))
 def=int(input("Pour défense ? (def)"))
 strength=int(input("Pour strength ? (str)"))
 luck=int(input("Pour luck ? (luck)"))
+
+#Specialities
 numb_dialogues=int(input("Nombre de dialogues voulues ?"))
 expneeded=int(input("Nombre d'exp voulue pour lvl up ? (de niveau 1 a 2). Le EXP necessaire pour lvl up pour les niveaux superieures sont proportionnels de 2"))
-expgivenbasique=int(input("Nombre d'exp obtenue en tuant un monstre basique ou en completant une quete simple ?"))
+expgivenbasic=int(input("Nombre d'exp obtenue en tuant un monstre basique ou en completant une quete simple ?"))
 expgivenunique=int(input("Nombre d'exp obtenue en tuant un monstre unique/boss ou en completant une quete epique ?"))
+cashgainedbasic=int(input("Nombre d'argent gagne lorsqu'on tue un monstre simple ou en completant une quete simple ?"))
+cashgainedunique=int(input("Nombre d'argent gagne lorsqu'on tue un monstre unique/boss ou en competant une quete epique ?"))
             
