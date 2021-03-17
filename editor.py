9a17341292298cc6f0887dffc6493a865d8d6a5b
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-17 15:47:58 +0100

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,10 +1,1 @@
-name=input("Votre nom ?")
 
-health=int(input("Pour health ? (HP)")
-invplace=int(input("Nombre de place dans l'inventaire ?")
-#Make list
-cash=int(input("Quantite d'argent au debut du jeu ?"))		 
-agi=int(input("Pour agilité ? (agi)"))
-def=int(input("Pour défense ? (def)"))
-strength=int(input("Pour strength ? (str)"))
-luck=int(input("Pour luck ? (luck)"))
