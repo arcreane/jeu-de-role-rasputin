311d09a724354705ea09b966f96dc7831e69623a
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-17 15:47:36 +0100

    Update editables.py

--- editables.py
+++ editables.py
@@ -1,1 +1,14 @@
-
+name=input("Votre nom ?")
+health=int(input("Pour health ? (HP)")
+invplace=int(input("Nombre de place dans l'inventaire ?")
+#Make list
+cash=int(input("Quantite d'argent au debut du jeu ?"))		 
+agi=int(input("Pour agilité ? (agi)"))
+def=int(input("Pour défense ? (def)"))
+strength=int(input("Pour strength ? (str)"))
+luck=int(input("Pour luck ? (luck)"))
+numb_dialogues=int(input("Nombre de dialogues voulues ?"))
+expneeded=int(input("Nombre d'exp voulue pour lvl up ? (de niveau 1 a 2). Le EXP necessaire pour lvl up pour les niveaux superieures sont proportionnels de 2"))
+expgivenbasique=int(input("Nombre d'exp obtenue en tuant un monstre basique ou en completant une quete simple ?"))
+expgivenunique=int(input("Nombre d'exp obtenue en tuant un monstre unique/boss ou en completant une quete epique ?"))
+            
