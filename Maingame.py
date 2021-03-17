c14ea4645a50174b74dce676b0e565dd0fbf0c3c
TheKiwi7 <80402029+TheKiwi7@users.noreply.github.com>
2021-03-17 15:52:26 +0100

    Update Maingame.py

--- Maingame.py
+++ Maingame.py
@@ -1,2 +1,15 @@
+import pygame
 print("Bienvenue dans le jeu dont vous etes le hero ! /n Vous etes actuellement en train de jouer le jeu cree par:", name)
-  
+
+def leftclick():
+pos = pygame.mouse.get_pos()
+for event in pygame.event.get()
+    if event.type == MOUSEBUTTONDOWN: #récupération des évenements lié à un clic de souris
+        if event.button == 1: # parmis ceux précédent je prend le clic gauche
+           x,y = pygame.mouse.get_pos()
+           collide = object_rect.collidepoint(x,y)
+           if collide:
+               return True
+           else:
+               return False
+          
