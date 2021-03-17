dc5db852169fe4fc5a2d25b745ec19d0d9bf17ee
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-17 15:01:15 +0100

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,1 +1,6 @@
-
+print("Bienvenue a ce créateur de jeu dont vous etes le héro !. Vous pouvez créer un jeu style RPG !" /n, "Commencons par le debut, determinons les stats de debut des personnages sans bonus :" )
+agi=int(input("Pour agilité ? (agi)"))
+def=int(input("Pour défense ? (def)"))
+strength=int(input("Pour strength ? (str)"))
+luck=int(input("Pour luck ? (luck)"))
+health=int(input("Pour health ? (HP)")
