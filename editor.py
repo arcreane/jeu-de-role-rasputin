78d9f8a202c1e65f637fd780777f10a63f89e0ca
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-17 15:31:53 +0100

    Update editor.py

--- editor.py
+++ editor.py
@@ -1,7 +1,8 @@
-print("Bienvenue a ce créateur de jeu dont vous etes le héro !. Vous pouvez créer un jeu style RPG !\nCommencons par le debut, determinons les stats de base des personnage :" )
-
+health=int(input("Pour health ? (HP)")
+invplace=int(input("Nombre de place dans l'inventaire ?")
+#Make list
+cash=int(input("Quantite d'argent au debut du jeu ?"))		 
 agi=int(input("Pour agilité ? (agi)"))
 def=int(input("Pour défense ? (def)"))
 strength=int(input("Pour strength ? (str)"))
 luck=int(input("Pour luck ? (luck)"))
-health=int(input("Pour health ? (HP)")
