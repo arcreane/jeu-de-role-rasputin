396e756c5780c9cd35598449560cfe689548ba0b
Thug2000 <35851118+Thug2000@users.noreply.github.com>
2021-03-31 15:17:39 +0200

    Update graphicseditor.py

--- graphicseditor.py
+++ graphicseditor.py
@@ -3,9 +3,7 @@ import tkinter as tk
 #creation de la fenetre editor tkinter
 editor = tk.Tk()
 editor.configure(bg='#92cbf7') #Couleur du background en bleu clair
-editor.geometry("1440x1080") #Taille page
-editor.minsize(width="1440", height="1080")
-editor.maxsize(width="1600", height="1200")
+editor.geometry("1080x720") #Taille page
 
 title2=tk.Label(text="Vous etes dans l'editeur du jeu", bg='#92cbf7', font=("Arial", 20, "bold"), fg="black")
 title2.pack(side="top", pady=50)
@@ -13,13 +11,17 @@ title2.pack(side="top", pady=50)
 frame1=tk.Frame(editor, bg='#fc9403')
 frame1.pack()
 
-stats=tk.Button(frame1, text="Changer les valeurs des Stats") #command=stats_edit_window
+photo_stats = tk.PhotoImage(file = r"./Plan-de-travail-6-150x150.png")
+photo_items =
+photo_dialogues = 
+
+stats=tk.Button(frame1, text="Changer les valeurs des Stats", image=photo_stats, compound="top") #command=stats_edit_window
 stats.grid(row="0", column="0", ipady=10, padx=10, pady=10)
 
 items=tk.Button(frame1, text="Ajouter des items et definir l'inventaire dans votre jeu") #command=items_edit_window
 items.grid(row="1", column="0", ipady=10, padx=10, pady=10)
 
 dialogues=tk.Button(frame1, text="Ajouter les dialogues dans votre jeu") #command=dialogues_edit_window
-dialogues.grid(row="0", column="1", ipady=10, padx=10, pady=10)
+dialogues.grid(row="2", column="0", ipady=10, padx=10, pady=10)
 
 editor.mainloop()
