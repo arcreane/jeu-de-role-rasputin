2201300ec3b9232d49b618d8a09f6369ed09e250
Sh0lf <80401462+Sh0lf@users.noreply.github.com>
2021-03-31 14:56:58 +0200

    Update graphicseditor.py

--- graphicseditor.py
+++ graphicseditor.py
@@ -9,3 +9,17 @@ editor.maxsize(width="1600", height="1200")
 
 title2=tk.Label(text="Vous etes dans l'editeur du jeu", bg='#92cbf7', font=("Arial", 20, "bold"), fg="black")
 title2.pack(side="top", pady=50)
+
+frame1=tk.Frame(editor, bg='#fc9403')
+frame1.pack()
+
+stats=tk.Button(frame1, text="Changer les valeurs des Stats") #command=stats_edit_window
+stats.grid(row="0", column="0", ipady=10, padx=10, pady=10)
+
+items=tk.Button(frame1, text="Ajouter des items et definir l'inventaire dans votre jeu") #command=items_edit_window
+items.grid(row="1", column="0", ipady=10, padx=10, pady=10)
+
+dialogues=tk.Button(frame1, text="Ajouter les dialogues dans votre jeu" #command=dialogues_edit_window
+dialogues.grid(row="0", column="1", ipady=10, padx=10, pady=10)
+
+editor.mainloop()
