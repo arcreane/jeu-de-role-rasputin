cee26994ee247dac3d0ff9c5e7361591ac65097c
TheKiwi7 <80402029+TheKiwi7@users.noreply.github.com>
2021-03-31 15:09:08 +0200

    Update GraphicsMenuGame.py

--- GraphicsMenuGame.py
+++ GraphicsMenuGame.py
@@ -2,22 +2,22 @@ import tkinter as tk
 
 master = tk.Tk()
 master.geometry("600x400")
-master.configure(bg="grey")
-
-title = tk.Label(master, text="Bienvenue sur le jeu", bg="cyan", font=("Arial", 20, "bold"))
-title.pack(side="top", pady=50)
+master.configure(bg="#b9b4b3")
 
 
-game = tk.Button(frame1, text="Charger sauvegarde", background="orange")
-game.grid(row="0", column="0", ipady=10, padx=10, pady=10)
+frame1=tk.Frame(master, bg='#b9b4b3')
+frame1.pack()
 
-editor = tk.Button(frame1, text="Nouvelle Partie", background="orange")
-editor.grid(row="0", column="1", ipady=10, padx=10, pady=10)
+cgame = tk.Button(frame1, text="Charger sauvegarde", background="#f3a421")
+cgame.grid(row="0", column="0", ipady=10, padx=10, pady=150)
+
+ngame = tk.Button(frame1, text="Nouvelle Partie", background="#f3a421")
+ngame.grid(row="0", column="1", ipady=10, padx=10, pady=150)
 
 menu_bar = tk.Menu(master)
 file_menu = tk.Menu(menu_bar, tearoff=0) #création du menu en haut de la page
 file_menu.add_command(label="Quitter", command=master.quit)
-menu_bar.add_cascade(label"Fichier", menu=file_menu)
+menu_bar.add_cascade(label="Fichier", menu=file_menu)
 master.config(menu=menu_bar)
 
 master.mainloop()
